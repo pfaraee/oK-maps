b49ee224090f3de42ad07d9a0852d4792c005cd3
pfaraee <pfaraee@gmail.com>
2017-03-04 17:18:39 -0800

    adds better testing and docs for BinaryFunctions, and changes color scheme

--- build/classes/BinaryFunctions.js
+++ build/classes/BinaryFunctions.js
@@ -8,45 +8,78 @@ exports.eliminateTerms = eliminateTerms;
 exports.solveGroup = solveGroup;
 exports.binaryTermToVarTerm = binaryTermToVarTerm;
 exports.getExpansionFormula = getExpansionFormula;
-//takes a number and returns a left padded binary string representation
+/*
+* Returns the binary representation of the number left padded to the number of vars
+* @param {number} num -  a number to be converted
+* @param {number} vars - the amount of vars the number should be represented in
+* @return {string} the binary representation of the number
+*/
 function decToBin(num, vars) {
-  if (num > Math.pow(2, vars) - 1) throw new Error('Error, num must be less than 2^vars');
+  if (num > Math.pow(2, vars) - 1 || num < 0) throw new Error('Invalid Number');
+
   var num = '' + num.toString(2);
   var pad = '0'.repeat(vars); // its just 5 0's for the max var nums
+
   return pad.substring(0, pad.length - num.length) + num;
 }
 
-//Takes two numbers represented as Binary Strings
-function eliminateTerms(num1, num2) {
-  num1 = num1.split('');
-  num2 = num2.split('');
+/*
+* Returns the result of applying Elimination Theorem to the two terms
+* @param {string} term1 - the first term
+* @param {string} term2 - the second term
+* @return {string} the result of Elimination Theorem
+*/
+function eliminateTerms(term1, term2) {
+  term1 = term1.split('');
+  term2 = term2.split('');
 
-  for (var i = 0; i < num1.length; i++) {
-    if (num1[i] != num2[i]) {
-      num1[i] = '-';
-      continue;
+  var numDiff = 0;
+
+  for (var i = 0; i < term1.length; i++) {
+    if (term1[i] != term2[i]) {
+      term1[i] = '-';
+      numDiff++;
+      if (numDiff > 1) throw new Error('Unsimplifiable Terms Given');
     }
   }
 
-  return num1.join('');
+  return term1.join('');
 }
 
+/*
+* Returns a group of terms solved using Elimination Theorem, represented as a string
+* @param {Array.Cell}  group - group to simplify
+* @param {number} vars - number of vars for the kmap
+* @return {string} the simplified group
+*/
 function solveGroup(group, vars) {
+  if (group.length & group.length - 1 && group.length != 1 || group.length > Math.pow(2, vars)) throw new Error("Invalid Group");
+
   var term1;
   var term2;
 
   if (group.length > 2) {
     term1 = solveGroup(group.slice(0, group.length / 2), vars);
     term2 = solveGroup(group.slice(group.length / 2), vars);
-  } else {
-    term1 = group[0].val;
-    term2 = group[1].val;
+    return eliminateTerms(term1, term2);
+  } else if (group.length < 2) {
+    return decToBin(group[0].val, vars);
   }
+
+  term1 = group[0].val;
+  term2 = group[1].val;
 
   return eliminateTerms(decToBin(term1, vars), decToBin(term2, vars));
 }
 
+/*
+* Returns a variable representation of a term
+* @param {string} term
+* @return {string} the convertedterm
+*
+*/
 function binaryTermToVarTerm(term) {
+  if (term === "") return "Undefined Term";
   term = term.split('');
   var string = "";
 
@@ -59,6 +92,12 @@ function binaryTermToVarTerm(term) {
   return string;
 }
 
+/*
+* Returns the expansion formula for the array of groups
+* @param {Array.Array.Cell} groups - groups to expand
+* @param {number} vars - number of vars in the map
+* @return {string} the expansionFormula
+*/
 function getExpansionFormula(groups, vars) {
   var formula = "F = ";
 
@@ -72,6 +111,7 @@ function getExpansionFormula(groups, vars) {
     }
 
     formula += binaryTermToVarTerm(term);
+
     if (i != groups.length - 1) formula += " + ";
   }
 
